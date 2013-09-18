3c2ede718629ea011c38a8d2bf4c2dd74e9e4eb1
sd-omkar <sd.omkar@gmail.com>
2013-09-18 12:40:27 -0500

    done sqort

--- hw2/sort.c
+++ hw2/sort.c
@@ -37,6 +37,14 @@ array[index2].key = temp;
 return;
 }
 
+// compare function for qsort
+int compare (const void *a, const void *b) {
+const entity *rec1 = a;
+const entity *rec2 = b;
+return ((int)(rec1->key) - (int)(rec2->key));
+}
+
+
 int main (int argc, char *argv[]) {
 // Check for valid input arguments
 if (argc != 3)
@@ -74,6 +82,7 @@ if (bytesRead != buf.st_size) {
 	perror("read");
  	exit(1);
 }
+close(fd);
 
 // Sort the array
 int i, smallestIndex;
@@ -84,18 +93,36 @@ for (i = 0; i < count; i++) {
 	swap_elements(randomArray + i, 0, smallestIndex);
 }
 gettimeofday(&end1, NULL);
-long int diff = (end1.tv_usec + 1000000 * end1.tv_sec) - (start1.tv_usec + 1000000 * start1.tv_sec);
+long int diff1 = (end1.tv_usec + 1000000 * end1.tv_sec) - (start1.tv_usec + 1000000 * start1.tv_sec);
 
+// Repeat for qsort
+fd = open(inputfile, O_RDONLY);
+if (fd < 0) {
+	perror("open");
+	exit(1);
+    }
+bytesRead = read(fd, randomArray, buf.st_size);
+if (bytesRead != buf.st_size) {
+	perror("read");
+ 	exit(1);
+}
+// Sort the array with qsort
+struct timeval start2, end2;
+gettimeofday(&start2, NULL);
+qsort(randomArray, count, sizeof(entity), compare);
+gettimeofday(&end2, NULL);
+long int diff2 = (end2.tv_usec + 1000000 * end2.tv_sec) - (start2.tv_usec + 1000000 * start2.tv_sec);
 
 // Print the required details
 //for (i = 0; i < count; i++) {
 //	printf("%d\n", randomArray[i].key);
 //}
-Printf("Selection sort metrics:\n");
+printf("Sort metrics:\n");
 printf("\tNumber of integers read = %d\n", count);
 printf("\tSmallest integer = %d\n", randomArray[0].key);
 printf("\tLargest integer = %d\n", randomArray[count-1].key);
-printf("\tTime spend sorting = %ldms\n", diff/1000);
+printf("\tTime spent in selection sort = %ldms\n", diff1/1000);
+printf("\tTime spent in quick sort = %ldms\n", diff2/1000);
 
 // Cleanup
 free(randomArray);
