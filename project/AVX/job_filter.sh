7258ba7f642410385101f0b016c277704d0951d1
omkar <sd.omkar@gmail.com>
2013-12-04 08:46:44 -0600

    Base, SSE, AVX timings

--- project/AVX/job_filter.sh
+++ project/AVX/job_filter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -N sse_filter
+#PBS -N cpu_filter
 #PBS -l nodes=1:ppn=1,walltime=10:00:00
-#PBS -d /home/odeshmukh/work/project/SSE
+#PBS -d /home/odeshmukh/work/project/Base
 
-for i in {1..14}
+for i in {8..24..8}
 do
   ./a.out -f $i -x 8192 -y 8192 >> log_filter
 done
--- project/Base/job_image.sh
+++ project/Base/job_image.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=1,walltime=10:00:00
 #PBS -d /home/odeshmukh/work/project/Base
 
-for i in {4000..12000..500}
+for i in {4000..16000..1000}
 do
-  ./a.out -f 9 -x $i -y $i >> log_image
+  ./a.out -f 16 -x $i -y $i >> log_image
 done
